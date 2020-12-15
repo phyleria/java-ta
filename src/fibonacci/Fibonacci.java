c24d69f7345fcd8de3ce7ebda581edcf57ad78d2
temilolakutelu <temilolakutelu@gmail.com>
2020-12-15 14:34:09 +0100

    java template

---
+++ src/fibonacci/Fibonacci.java
@@ -0,0 +1,41 @@
+
+package fibonacci;
+ 
+public class Fibonacci {
+
+     
+    
+    public static int getresult(int n) 
+    {
+        if (n < 0)
+        {
+            throw new IllegalArgumentException("n must be >= 0");
+        }
+	else if (n < 2)
+	{
+	    return n;
+	}
+	else 
+	{
+	    return getresult(n - 1) + getresult(n - 2);
+	}
+    }
+
+    
+
+    public static void main(String[] args) 
+    {
+        if (args.length < 1)
+        {
+             System.exit(1);
+        }
+        int n = Integer.parseInt(args[0]);
+        if (n < 0)
+        {
+             System.exit(2);
+        }
+        int fibonacci = Fibonacci.getresult(n);
+        System.out.print("Fibonacci(" + n + ") = " + fibonacci + "\n");
+    }
+    
+}
